1fc494cc4e374376c0e3179549ba3cca2ace6622
Bianca Currie <270021658@bun2-206-16.acg.ac.nz>
2021-06-01 11:56:36 +1200

    conditionals update

--- assets/custom.js
+++ assets/custom.js
@@ -9,12 +9,24 @@
 
   var updateImg = document.querySelector('.card img');
 
+  //1. get the image input
+  var getImageSelect = document.getElementById('image');
+  console.log(getImageSelect);
 
+  //2. get the image input value
 
 
 // Function for button click update
 function updateChoices (){
-  updateImg.src = 'assets/christmas.jpg';
+  var getImageSelectVal = getImageSelect.value;
+  if (getImageSelectVal === 'christmas') {
+      updateImg.src = 'assets/christmas.jpg';
+  } else if (getImageSelectVal === 'birthday') {
+    updateImg.src = 'assets/birthday.jpg';
+  } else if (getImageSelectVal === 'valentines'){
+    updateImg.src = 'assets/valentines.jpg';
+    }
+  console.log(getImageSelectVal);
 };
 
 
